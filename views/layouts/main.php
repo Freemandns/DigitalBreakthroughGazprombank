16b243798ba00e434b97635d26efa4e022255f2e
Игорь Никифоров <iigornnikiforov@gmail.com>
2020-06-20 17:02:20 +0300

    design

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,6 +9,7 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
+use yii\helpers\Url;
 
 AppAsset::register($this);
 ?>
@@ -19,6 +20,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/web/assets/main_css/style.css" rel="stylesheet" type="text/css">
     <?php $this->registerCsrfMetaTags() ?>
     <title>ПРОДВИЖЕНИЕ ИДЕЙ: <?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
@@ -27,53 +29,87 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Главная', 'url' => ['/site/index']],
-            ['label' => 'Личный кабинет', 'url' => ['/site/lk']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Выход',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
+    <header> 
+    <a href="index.html"><img src="/web/assets/main_img/logobank.png"></a>
+    <!-- <ul class='menu'>
+        <a href="#"><li>Личный кабинет</li></a>
+        <a href="#"><li>Популярные идеи</li></a>
+        <a href="#"><li>Тематики</li></a>
+        <a href="#"><li>Рейтинг Участников</li></a>
+        <a href="#"><li>Случайные идеи</li></a>
+        
+    </ul> -->
 
-    <div class="container">
+    <ul class="menu">
+        <?php
+            NavBar::begin([
+                'brandUrl' => Yii::$app->homeUrl,
+            ]);
+            echo Nav::widget([
+                'options' => ['class' => 'asd'],
+                'items' => [
+                    // ['label' => 'Главная', 'url' => ['/site/index']],
+                    ['label' => 'Личный кабинет', 'url' => ['/site/lk']],
+                    ['label' => 'Популярные идеи', 'url' => '#'],
+                    ['label' => 'Тематики', 'url' => '#'],
+                    ['label' => 'Рейтинг участников', 'url' => '#'],
+                    ['label' => 'Случайные идеи', 'url' => '#'],
+                    Yii::$app->user->isGuest ? (
+                        ['label' => 'Авторизация', 'url' => ['/site/login']]
+                    ) : (
+                        Html::beginForm(['/site/logout'], 'post')
+                        . Html::submitButton(
+                            'ВЫХОД',
+                            ['class' => 'asdafg']
+                        )
+                        . Html::endForm()
+                    )
+                ],
+            ]);
+            NavBar::end();
+        ?>
+    </ul>
+    </header>
+
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
 			'homeLink'=>['label'=>'Главная','url'=>'/web/'],
         ]) ?>
         <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
+
+            <?= $content ?>
+
 </div>
 
-<footer class="footer">
+
+<footer>
+<div class='menu_footer'>
+    <div id='footer_menu'>
+        <?php 
+            echo "<a class='menu_btn' href='/web/index.php?r=site%2Flk'>Личный кабинет</a><br>
+                  <a class='menu_btn' href='#'>Популярные идеи</a><br>
+                  <a class='menu_btn' href='#'>Тематики</a><br>
+                  <a class='menu_btn' href='#'>Рейтинг участников</a><br>
+                  <a class='menu_btn' href='#'>Слуйчайные идеи</a><br>"; 
+        ?> 
+    </div>
+    <div class='soccet'>
+        <a href="#"><img class='fix_size_ico' src="/web/assets/main_img/in.png"></a>
+        <a href="#"><img class='fix_size_ico' src="/web/assets/main_img/f.png"></a>
+        <a href="#"><img class='fix_size_ico' src="/web/assets/main_img/vk.png"></a>
+    </div>
+</div>
+<hr>
+<div class='copy'>Все права защищены. 2020 © ЖЕСТЬ </div>
+</footer>
+
+<!-- <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; ПРОДВИЖЕНИЕ ИДЕЙ <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
-</footer>
+</footer> -->
 
 <?php $this->endBody() ?>
 </body>
